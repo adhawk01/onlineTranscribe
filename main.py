fee9fa17ddc097c20ba7b042fbe0381aa3405125
adhawk01 <gabay417@gmail.com>
2025-11-23 08:39:45 -0800

    add unicorn + https

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from asgiref.wsgi import WsgiToAsgi
 
 app = Flask(__name__)
 
@@ -8,5 +9,15 @@ def root():
     return "Hello From Flask!"
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5001, host="0.0.0.0")
+asgi_app = WsgiToAsgi(app)
+
+# if __name__ == "__main__":
+#     app.run(
+#         debug=True,
+#         port=5001,
+#         host="0.0.0.0",
+#         ssl_context=(
+#             r"C:\ssl\quicktranslate.online-chain.pem",   # certificate (or fullchain)
+#             r"C:\ssl\quicktranslate.online-key.pem"    # private key
+#         )
+#     )
